933d2b7837d731506d97f25c33a9a9ddd1ef3970
angelxescomx08 <ah@xdevelop.mx>
2022-08-13 13:43:18 -0500

    cosas del contexto

---
+++ context/ui/UIProvider.tsx
@@ -0,0 +1,27 @@
+import { FC, useReducer } from 'react';
+import { UIContext, uiReducer } from './';
+
+interface Props {
+    children: JSX.Element
+}
+
+export interface UIState {
+    sideMenuOpen: boolean;
+}
+
+const UI_INITIAL_STATE: UIState = {
+    sideMenuOpen: false,
+}
+
+export const UIProvider: FC<Props> = ({ children }) => {
+
+    const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE)
+
+    return (
+        <UIContext.Provider value={{
+            sideMenuOpen: false
+        }}>
+           {children}
+        </UIContext.Provider>
+    )
+}
