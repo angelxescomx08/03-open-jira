6538ec770af70a21a8f056875e3da3730d200854
angelxescomx08 <ah@xdevelop.mx>
2022-08-27 14:06:37 -0500

    Contexto para entradas y diseño

---
+++ context/entries/EntriesProvider.tsx
@@ -0,0 +1,48 @@
+import { FC, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Entry } from '../../interfaces';
+import { EntriesContext, entriesReducer } from './';
+
+interface Props {
+    children: JSX.Element
+}
+
+export interface EntriesState {
+    entries: Entry[];
+}
+
+const Entries_INITIAL_STATE: EntriesState = {
+    entries: [
+        {
+            _id: uuidv4(),
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed blanditiis facilis quidem nobis quasi totam ullam suscipit asperiores nisi pariatur aspernatur illo voluptates, quis commodi architecto? Qui, magnam consequuntur. Aliquam.',
+            status: 'pending',
+            createdAt: Date.now()
+        },
+        {
+            _id: uuidv4(),
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed blanditiis facilis quidem nobis quasi totam ullam suscipit asperiores nisi pariatur aspernatur illo voluptates, quis commodi architecto? Qui, magnam consequuntur. Aliquam.',
+            status: 'in-progress',
+            createdAt: Date.now() - 50004
+        },
+        {
+            _id: uuidv4(),
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed blanditiis facilis quidem nobis quasi totam ullam suscipit asperiores nisi pariatur aspernatur illo voluptates, quis commodi architecto? Qui, magnam consequuntur. Aliquam.',
+            status: 'finished',
+            createdAt: Date.now() - 88888
+        }
+    ],
+}
+
+export const EntriesProvider: FC<Props> = ({ children }) => {
+
+    const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE)
+
+    return (
+        <EntriesContext.Provider value={{
+            ...state
+        }}>
+           {children}
+        </EntriesContext.Provider>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
-import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
+
+import { Card, CardContent, CardHeader, Grid, Typography } from '@mui/material'
+
 import { Layout } from '../components/layouts'
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>Hola mundo</Typography>
+    <Layout title='Home - OpenJira'>
+      <Grid container spacing={2}>
+
+        <Grid item xs={12} sm={4}  >
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title="Pendientes" />
+
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}  >
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title="En progreso" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}  >
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title="Completadas" />
+          </Card>
+        </Grid>
+
+      </Grid>
     </Layout>
   )
 }
