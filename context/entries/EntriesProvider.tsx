f6832ae754167f06870088e9d9f03353aaac24e8
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-21 19:36:40 -0600

    crear una entrada desde el front y grabarla en la base de datos

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -20,17 +20,14 @@ export const EntriesProvider: FC<Props> = ({ children }) => {
 
     const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE)
 
-    const addNewEntry = (description: string) => {
-        const newEntry: Entry = {
-            _id: uuidv4(),
-            description,
-            createdAt: Date.now(),
-            status: 'pending'
-        }
+    const addNewEntry = async (description: string) => {
+        const { data } = await entriesApi.post<Entry>('/entries', {
+            description
+        })
 
         dispatch({
             type: '[Entry] - Add-Entry',
-            payload: newEntry
+            payload: data
         })
     }
 
