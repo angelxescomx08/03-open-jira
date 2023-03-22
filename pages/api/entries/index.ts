e5fb3e29ffae1792e0fc9defa4030279ff4bf47d
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-21 19:28:24 -0600

    crear el endpoitn de nueva entrada

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -12,6 +12,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
     switch (req.method) {
         case 'GET':
             return getEntries(res)
+        case 'POST':
+            return postEntrie(req, res)
         default:
             res.status(400).json({ message: 'Endpoint no existe' })
     }
@@ -25,4 +27,30 @@ const getEntries = async (res: NextApiResponse<Data>) => {
     await db.disconect()
 
     return res.status(200).json(entries)
+}
+
+const postEntrie = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const { description = '' } = req.body;
+
+    const newEntrie = new Entry({
+        description,
+        createdAt: Date.now()
+    })
+
+    try {
+        await db.conect()
+
+        await newEntrie.save()
+
+        await db.disconect()
+
+        res.status(200).json(newEntrie)
+    } catch (error) {
+        await db.disconect()
+        console.log(error);
+        res.status(500).json({ message: 'Algo salió mal al crear la entrada' })
+    }
+
+
 }
